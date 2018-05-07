d6d5fe86b523f9f0d2b6b560b98992568f3dda3e
PeijieLi <li_paige@berkeley.edu>
2018-05-06 20:51:05 -0700

    Add timeline to website

--- index.php
+++ index.php
@@ -48,85 +48,46 @@ function flipBillImage(id) {
             <div class="col-md-12">
                 <h2><font color="white">Abstract</font></h2>
                 <p><font color="white">
-                    &emsp;&emsp;&emsp;&emsp;This project attempts to examine the current meat industry through the lens of 
-                    animal agriculture legislation. Specifically, researchers set out to determine whether 
-                    the current animal agriculture system is working, based on the implications on human 
-                    health, environmental sustainability, labor conditions, and animal welfare. We also 
-                    aim to identify the barriers to achieving effective animal welfare legislation.<br>
+                    &emsp;&emsp;&emsp;&emsp;This project attempts to examine the current meat industry through 
+                    the lens of animal agriculture legislation. Specifically, researchers set out to determine whether 
+                    the current animal agriculture system is working, based on the implications on human health, environmental 
+                    sustainability, labor conditions, and animal welfare. We also aim to identify the barriers to achieving 
+                    effective animal welfare legislation.<br>
                     &emsp;&emsp;&emsp;&emsp;The legislation we have studied demonstrates this systematic failure. Antibiotic use 
-                    in animal agriculture is causing the rapid development of untreatable “superbugs,” 
-                    and may even be causing the increase of obesity that the United States has seen 
-                    since the 1950s. Upon examining the history of federal food safety laws, it becomes 
-                    apparent that intensified animal agriculture has required more stringent food safety 
-                    laws to protect consumers from bacteria. Unfortunately, federal food safety laws overlook 
-                    the relationship between animal welfare and human health, and there have been several failed 
-                    attempts to pass laws and amendments to existing laws to improve conditions for farm animals. 
-                    Prevent Cruelty California, while it may create a minute increase in the space allocated for 
-                    each animal, has negligibly weak enforcement mechanisms. Lobbying from corporate interest groups, 
-                    like Big Meat and Big Pharma, challenge the implementation of effective legislation to 
-                    improve conditions for farm animals. Although the Humane Slaughter Act attempted to provide 
-                    more humane conditions for agricultural animals, it has failed to protect poultry and has 
-                    failed to enforce the policies.[Briefly describe the findings from other legislation here].<br>
+                    in animal agriculture is causing the rapid development of untreatable “superbugs,” and may even be causing 
+                    the increase of obesity that the United States has seen since the 1950s. Upon examining the history of federal 
+                    food safety laws, it becomes apparent that intensified animal agriculture has required more stringent food safety 
+                    laws to protect consumers from bacteria. Unfortunately, federal food safety laws overlook the relationship between 
+                    animal welfare and human health, and there have been several failed attempts to pass laws and amendments to existing 
+                    laws to improve conditions for farm animals. Prevent Cruelty California, while it may create a minute increase in the 
+                    space allocated for each animal, has negligibly weak enforcement mechanisms. Lobbying from corporate interest groups, 
+                    like Big Meat and Big Pharma, challenge the implementation of effective legislation to improve conditions for farm animals. 
+                    Although the Humane Slaughter Act attempted to provide more humane conditions for agricultural animals, it has failed to 
+                    protect poultry and has failed to enforce the policies. <br>
                     &emsp;&emsp;&emsp;&emsp;Likewise, interviews we have conducted show mistrust of the animal agriculture industry 
-                    among the general public. Many people are opting to withdraw their monetary support from 
-                    the industry altogether, despite the social challenges that such actions may impose. 
-                    Additionally, we found that animal welfare legislation in the United States is very weak 
-                    in terms of protecting agricultural animals. The United States lack concrete legislations 
-                    for agricultural animals and has fueled the expansion of big agribusiness by ignoring animal 
-                    welfare.   [Briefly describe the findings from other interviews here.]<br>
-                    &emsp;&emsp;&emsp;&emsp;Although the survey was somewhat limited and we recognize that we only surveyed a 
-                    small population, we had some interesting findings. For example, 84% of people selected 
-                    environmental welfare as their general reason behind voting, whereas only 75% selected 
-                    animal welfare. Similarly, we found that many people said that they were more inclined 
-                    to vote for animal welfare legislation if it addressed other issues, especially environmental 
-                    health and human health. [Briefly describe findings from survey results here.]
-
+                    among the general public. Many people are opting to withdraw their monetary support from the industry altogether, 
+                    despite the social challenges that such actions may impose. Additionally, we found that animal welfare legislation 
+                    in the United States is very weak in terms of protecting agricultural animals. The United States lack concrete 
+                    legislations for agricultural animals and has fueled the expansion of big agribusiness by ignoring animal welfare. 
+                    Although the survey was somewhat limited and we recognize that we only surveyed a small population, we had some 
+                    interesting findings. For example, 84% of people selected environmental welfare as their general reason behind voting, 
+                    whereas only 75% selected animal welfare. Similarly, we found that many people said that they were more inclined to vote 
+                    for animal welfare legislation if it addressed other issues, especially environmental health and human health. <br>
+                    &emsp;&emsp;&emsp;&emsp;Katie and Kalena worked on the Humane Slaughter Act podcast and interviewed a graduate student 
+                    in the Goldman School of Public Policy for the podcast. Bri wrote three podcasts: Introduction + Framing Animal Welfare 
+                    Legislation, Federal Food Safety and Animal Welfare, and Lobbying. She also interviewed students about their perceptions 
+                    of animal welfare to supplement the framing podcast. Kalena worked on the Animal Welfare Legislation Demographics survey. 
+                    Heliya conducted research for, wrote, produced, edited, and interviewed people for the Antibiotics and Prevent Cruelty 
+                    California podcast. Heliya also submitted 10 additional Meet and Greet interviews, and helped organize survey results. 
+                    Alicia worked on exploring what sustainable meat means by interviewing Monica Rocchino, a local butcher in Berkeley who 
+                    believes in sourcing meat from sustainable farms, and creating a podcast addressing this. Alicia also created the 
+                    “More Than Meat” logo for the website. Peijie created and edited the website. <br>
+                    &emsp;&emsp;&emsp;&emsp;In short, our interviews, surveys, research, podcast, and website demonstrate failure 
+                    to protect human health, environmental sustainability, labor conditions, and animal welfare. We conclude 
+                    that the current system of animal agriculture is working poorly. <br>
                 </font></p>
             </div>
         </div>
 </body>
 
-        <!-- <div class="row">
-            <a class="anchor" id="podcasts" > </a>
-
-            <div class="col-md-12">
-                <h2>Podcasts</h2>
-
-
-
-            </div>
-            <a href="podcast-animalwelfare.php">Animal Welfare</a><br>
-            <a href="podcast-antibiotics.php"><img id="antibiotics" src="img/01-blog-antibiotics-FB.jpg" height="200px" class="img-rounded"/></a> <br>
-            <a href="podcast-foodsafety_animalagriculture.php">Food Safety and Animal Agriculture</a><br>
-            <a href="podcast-humaneslaughter.php">Humane Slaughter</a><br>
-            <a href="podcast-preventcrueltyca.php">Prevent Cruelty California</a><br>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/zomhCOq9YUQ" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-            
-        </div> -->
-
-        
-<!-- 
-        <div class="row">
-            <a class="anchor" id="resources" > </a>
-
-            <div class="col-md-12">
-                <h2>Resources</h2>
- <table cellSpacing="2" cellPadding="2" class="table table-bordered table-striped" >
-  <tr>
-    <td>List Resources Here:
-    &nbsp;
-
-  <tr>
-    <td>List Resources Here:
-    &nbsp;
-  </tr>
-
-</table>
-        </div>
-    </div>
- -->
-
 <?php include('pagefooter.html'); ?>
-<!-- 
-  </body>
-</html> -->
--- timeline.php
+++ timeline.php
@@ -1,16 +1,31 @@
 <?php include('pageheader.html'); ?>
 
 <?php
-$rows = array(array("2018", "Event at 2018", "Description of event at 2018", "img/team/peijie.jpg"),
-              array("2017", "Event at 2017", "Description of event at 2017", "img/team/no-image.jpg"),
-              array("2016", "Event at 2016", "Description of event at 2016", "img/team/peijie.jpg"));
+$rows = array(
+              // array("", "Event at 2018", "Description of event at 2018", "img/team/peijie.jpg"),
+              // array("2017", "Event at 2017", "Description of event at 2017", "img/team/no-image.jpg"),
+              // array("2017", "Event at 2017", "Description of event at 2017", "img/team/no-image.jpg"),
+              // array("2017", "Event at 2017", "Description of event at 2017", "img/team/no-image.jpg"),
+              // array("2017", "Event at 2017", "Description of event at 2017", "img/team/no-image.jpg"),
+              // array("2017", "Event at 2017", "Description of event at 2017", "img/team/no-image.jpg"),
+              array("1958-1963", "Dr. Gerry Combs", "Professor, Dr. Gerry Combs, develops the nutritional principle of calorie:protein ratio and later calorie:amino acid ratios for use in commercial broiler diets. Rapid adoption of this principle along with the use of least cost diet formulation spurs the development of the poultry industry on Maryland’s Eastern Shore.", "img/team/no-image.jpg"),
+              array("1947", "Maryland Artificial Breeders Cooperative", "Maryland Artificial Breeders Cooperative (MABC) founded. MABC is one of 10 founding cooperatives that form Select Sire, an international dairy artificial insemination bull stud.", "img/team/no-image.jpg"),
+              array("1937", "Depression era", "New campus livestock facilities constructed through Depression era “Works Progress Administration Program.” The last remaining buildings form the current “Campus Farm.”", "img/team/no-image.jpg"),
+              array("1914", "Smith-Lever Act", "Smith-Lever Act encourages land-grant colleges to establish community education programs in agriculture and home economics. Results in formation of the Maryland Cooperative Extension Service.", "img/team/no-image.jpg"),
+              array("1888", "Hatch Act", "Congress passes Hatch Act creating federally funded agricultural experiment stations. Maryland Agricultural Experiment Station established using The Rossborough Inn as its first headquarters. ", "img/team/no-image.jpg"),
+              array("1864", "Land Grant Act", "President Lincoln signs the Morrill Land Grant Act providing federal support for state colleges to teach agriculture, mechanical arts and military tactics. According to formula, Maryland receives 210,000 acres of “Western” land under this legislation", "img/team/no-image.jpg"),
+
+              array("1862", "Public Land for Agricultural Education", "Land Grant Act provides public lands for universities to teach agriculture7", "img/team/no-image.jpg"),
+              array("1861-1865", "Civil War", "Before the war, pork was the most popular meat (mainly due to ease of storage), but beef became the most popular meat after the war because of refrigeration. ", "img/team/no-image.jpg"),
+              array("1760", "Breeding Animals", "Robert Bakewell, England, began breeding animals for a purpose, created different types", "img/team/no-image.jpg"),
+              array("1641", "First Meatpacker", "William Pynchon, Massachusetts, was first meatpacker exporting salt pork", "img/team/no-image.jpg"),
+              array("1539", "DeSoto: horses", "DeSoto brought horses and hogs to Florida", "img/team/no-image.jpg"),
+              array("1519", "Cattle + Sheep", "Cortez brought cattle and sheep to Mexico", "img/team/no-image.jpg"),
+              array("1943", "Livestock", "Columbus brought livestock to the West Indies", "img/team/peijie.jpg"));
 ?>
 
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<html class="no-js">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -22,11 +37,6 @@ $rows = array(array("2018", "Event at 2018", "Description of event at 2018", "im
         <link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
     </head>
     <body>
-        <!--[if lt IE 7]>
-            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
-
-        <!-- Add your site or application content here -->
         <div class="container">
             <div class="over-wrap-index">
                 <div id="wrap-index" class="wrap-index">
@@ -62,187 +72,3 @@ $rows = array(array("2018", "Event at 2018", "Description of event at 2018", "im
 
     </body>
 </html>
-
-<!-- <!DOCTYPE html>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<style>
-* {
-    box-sizing: border-box;
-}
-
-body {
-    background-color: #474e5d;
-    font-family: Helvetica, sans-serif;
-}
-
-/* The actual timeline (the vertical ruler) */
-.timeline {
-    position: relative;
-    max-width: 1200px;
-    margin: 0 auto;
-}
-
-/* The actual timeline (the vertical ruler) */
-.timeline::after {
-    content: '';
-    position: absolute;
-    width: 6px;
-    background-color: white;
-    top: 0;
-    bottom: 0;
-    left: 50%;
-    margin-left: -3px;
-}
-
-/* Container around content */
-.container {
-    padding: 10px 40px;
-    position: relative;
-    background-color: inherit;
-    width: 50%;
-}
-
-/* The circles on the timeline */
-.container::after {
-    content: '';
-    position: absolute;
-    width: 25px;
-    height: 25px;
-    right: -17px;
-    background-color: white;
-    border: 4px solid #FF9F55;
-    top: 15px;
-    border-radius: 50%;
-    z-index: 1;
-}
-
-/* Place the container to the left */
-.left {
-    left: 0;
-}
-
-/* Place the container to the right */
-.right {
-    left: 50%;
-}
-
-/* Add arrows to the left container (pointing right) */
-.left::before {
-    content: " ";
-    height: 0;
-    position: absolute;
-    top: 22px;
-    width: 0;
-    z-index: 1;
-    right: 30px;
-    border: medium solid white;
-    border-width: 10px 0 10px 10px;
-    border-color: transparent transparent transparent white;
-}
-
-/* Add arrows to the right container (pointing left) */
-.right::before {
-    content: " ";
-    height: 0;
-    position: absolute;
-    top: 22px;
-    width: 0;
-    z-index: 1;
-    left: 30px;
-    border: medium solid white;
-    border-width: 10px 10px 10px 0;
-    border-color: transparent white transparent transparent;
-}
-
-/* Fix the circle for containers on the right side */
-.right::after {
-    left: -16px;
-}
-
-/* The actual content */
-.content {
-    padding: 20px 30px;
-    background-color: white;
-    position: relative;
-    border-radius: 6px;
-}
-
-/* Media queries - Responsive timeline on screens less than 600px wide */
-@media screen and (max-width: 600px) {
-  /* Place the timelime to the left */
-  .timeline::after {
-    left: 31px;
-  }
-  
-  /* Full-width containers */
-  .container {
-    width: 100%;
-    padding-left: 70px;
-    padding-right: 25px;
-  }
-  
-  /* Make sure that all arrows are pointing leftwards */
-  .container::before {
-    left: 60px;
-    border: medium solid white;
-    border-width: 10px 10px 10px 0;
-    border-color: transparent white transparent transparent;
-  }
-
-  /* Make sure all circles are at the same spot */
-  .left::after, .right::after {
-    left: 15px;
-  }
-  
-  /* Make all right containers behave like the left ones */
-  .right {
-    left: 0%;
-  }
-}
-</style>
-</head>
-<body>
-
-<div class="timeline">
-  <div class="container left">
-    <div class="content">
-      <h2>2017</h2>
-      <p>Event</p>
-    </div>
-  </div>
-  <div class="container right">
-    <div class="content">
-      <h2>2016</h2>
-      <p>Event</p>
-    </div>
-  </div>
-  <div class="container left">
-    <div class="content">
-      <h2>2015</h2>
-      <p>Event</p>
-    </div>
-  </div>
-  <div class="container right">
-    <div class="content">
-      <h2>2012</h2>
-      <p>Event</p>
-    </div>
-  </div>
-  <div class="container left">
-    <div class="content">
-      <h2>2011</h2>
-      <p>Event</p>
-    </div>
-  </div>
-  <div class="container right">
-    <div class="content">
-      <h2>2007</h2>
-      <p>Event</p>
-    </div>
-  </div>
-</div>
-
-</body>
-</html> -->
