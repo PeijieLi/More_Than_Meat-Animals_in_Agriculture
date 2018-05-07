fc7b3e7f5f7bd641ced866d7413c1c2aed2c78e1
PeijieLi <li_paige@berkeley.edu>
2018-05-06 19:16:12 -0700

    finalize podcasts page

--- Podcasts.php
+++ Podcasts.php
@@ -19,14 +19,37 @@
 <div class="vc-column wpb_column vc_column_container vc_col-sm-12 vc_col-lg-6"><div class="vc_column-inner "><div class="wpb_wrapper">
 	<blockquote>
 		<img style="float:left;width:200px;height:270px;margin-right:20px;" src="img/team/no-image.jpg" class="attachment-thumbnail size-thumbnail" alt="Heliya Izadpanah">	
-		<h3><font color="white">More Than Meat - Prevent Cruelty California</font></h3><br>
+		<h3><font color="white">More Than Meat - Prevent Cruelty California (Part 1) </font></h3><br>
 		<audio controls>
 		  <source src="audios/prevent_cruelty.wav" type="audio/wav">
 		Your browser does not support the audio element.
 		</audio>
 
 		<p><font color="white">
-		I’m Heliya, and you’re listening to More Than Meat, a podcast series on Animal Agriculture legislation. 
+		&emsp;&emsp;&emsp;&emsp;I’m Heliya, and you’re listening to More Than Meat, a podcast series on Animal Agriculture legislation. 
+		Today we will be discussing Prevent Cruelty California, a bill intended to close the loopholes that 
+		were left open on Proposition 2. Proposition 2, which passed in 2008, aimed to outlaw battery cages in California factory farms, 
+		and mandated that animals kept at these facilities must, at a minimum, be granted sufficient 
+		space to stretch, turn around, and lie down. However, since the passage of the proposition, the 
+		California Department of Food and Agriculture has taken no measures to enforce the law prescribed 
+		by Proposition 2. Hence, many animal welfare organizations, and their allies, have led a canvassing 
+		campaign. As of April 24th, Prevent Cruelty California has received over 600,000 signatures, ensuring 
+		that a spot for the Bill on the November 2018 ballot. Then it will be up to Californian Voters to 
+		decide if they want the bill to pass or not.  
+		</font></p>
+	</blockquote>
+	</div></div></div>
+<div class="vc-column wpb_column vc_column_container vc_col-sm-12 vc_col-lg-6"><div class="vc_column-inner "><div class="wpb_wrapper">
+	<blockquote>
+		<img style="float:left;width:200px;height:270px;margin-right:20px;" src="img/team/no-image.jpg" class="attachment-thumbnail size-thumbnail" alt="Heliya Izadpanah">	
+		<h3><font color="white">More Than Meat - Prevent Cruelty California (Part 2) </font></h3><br>
+		<audio controls>
+		  <source src="audios/prevent_cruelty1.wav" type="audio/wav">
+		Your browser does not support the audio element.
+		</audio>
+
+		<p><font color="white">
+		&emsp;&emsp;&emsp;&emsp;I’m Heliya, and you’re listening to More Than Meat, a podcast series on Animal Agriculture legislation. 
 		Today we will be discussing Prevent Cruelty California, a bill intended to close the loopholes that 
 		were left open on Proposition 2. Proposition 2, which passed in 2008, aimed to outlaw battery cages in California factory farms, 
 		and mandated that animals kept at these facilities must, at a minimum, be granted sufficient 
@@ -50,7 +73,7 @@
 		</audio>
 
 		<p><font color="white">
-		Hello, welcome to another episode of More than Meat. Today, we are going to explore the history 
+		&emsp;&emsp;&emsp;&emsp;Hello, welcome to another episode of More than Meat. Today, we are going to explore the history 
 		of federal food safety laws and identify human health as a driving point for livestock-related 
 		legislation. Many of the federal laws pertaining animal agriculture are related to food safety. 
 		This is understandably the case, as most people are concerned about the health implications of 
@@ -69,7 +92,7 @@
 		</audio>
 
 		<p><font color="white">
-		Hello, I’m Heliya Izadpanah, and you’re listening to More Than Meat, a podcast series on Animal 
+		&emsp;&emsp;&emsp;&emsp;Hello, I’m Heliya Izadpanah, and you’re listening to More Than Meat, a podcast series on Animal 
 		Agriculture legislation. Today we will be discussing antibiotic use on factory farms, their impact 
 		on human populations, and relevant lobbying and legislation. <br>
 		The majority of antibiotics on the market today are used on “farm” animals living in factory farms. 
@@ -89,11 +112,11 @@
 		<img style="float:left;width:200px;height:200px;margin-right:20px;" src="img/podcasts/Local_Butcher_Shop_PIC.jpeg" class="attachment-thumbnail size-thumbnail" alt="Heliya Izadpanah">	
 		<h3><font color="white">The Local Butcher Shop</font></h3><br>
 		<audio controls>
-	  	  <source src="audios/The_Local_Butcher_Shop.mp3" type="audio/mpeg"> 
+	  	  <source src="audios/local_butcher.mp3" type="audio/mpeg"> 
 		</audio>
 
 		<p><font color="white">
-		This is More Than Meat, I’m Alicia Lai. Previously, we explored in depth about the problematic 
+		&emsp;&emsp;&emsp;&emsp;This is More Than Meat, I’m Alicia Lai. Previously, we explored in depth about the problematic 
 		treatment of animals and workers in the current meat industry. You may be wondering what you could 
 		do as a consumer to address these issues. This is a complex question with many different answers. 
 		One solution is to support your local butcher. In this podcast, we interviewed Monica Rocchino, 
@@ -115,7 +138,7 @@
 		</audio>
 
 		<p><font color="white">
-		Human Slaughter Podcast<br><br><br><br>
+		&emsp;&emsp;&emsp;&emsp;Human Slaughter Podcast<br><br><br><br>
 
 		</font></p>
 	</blockquote>
@@ -130,7 +153,7 @@
 		</audio>
 
 		<p><font color="white">
-		Introduction: <br><br><br><br>
+		&emsp;&emsp;&emsp;&emsp;Introduction: <br><br><br><br>
 
 		</font></p>
 	</blockquote>
